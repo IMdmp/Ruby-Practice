4994f0c9177f10127cd2373059318851db7a39da
Dominic Pagtalunan <pagtalunan.d@gmail.com>
2021-12-09 22:18:47 +0800

    fix slight wrap bug

--- caesarcypher.rb
+++ caesarcypher.rb
@@ -13,9 +13,9 @@ def caesar_cipher(message,shiftFactor)
 
     message.each_char do |x| 
         if upp_case_arr.include?(x) 
-            res_string += upp_case_arr[(upp_case_arr.find_index(x) + shiftFactor) % 25]
+            res_string += upp_case_arr[(upp_case_arr.find_index(x) + shiftFactor) % 26]
         elsif (low_case_arr.include?(x))
-            res_string += low_case_arr[(low_case_arr.find_index(x) + shiftFactor) % 25]
+            res_string += low_case_arr[(low_case_arr.find_index(x) + shiftFactor) % 26]
         else
             res_string += x
         end 
@@ -30,5 +30,5 @@ def caesar_cipher(message,shiftFactor)
 end
 
 
-caesar_cipher("What a string!",5)
+caesar_cipher("What w string!",5)
 
