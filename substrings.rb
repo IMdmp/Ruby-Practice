83aa37ee7b2239bc945c0c62a9e101d2c1fc3609
Dominic Pagtalunan <pagtalunan.d@gmail.com>
2021-12-14 17:29:14 +0800

    substrings working

---
+++ substrings.rb
@@ -0,0 +1,20 @@
+def substrings(word, sub_string)
+    word.split(" ").each{ |word_item| 
+        puts word_item
+    }
+
+    sub_string.reduce(Hash.new(0)){  | result, substring | 
+        word.split(" ").each{ |word_item| 
+            if word_item.include?(substring) then 
+                result[substring] +=1
+            end 
+            
+        }
+        result
+    }          
+end
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+    
+
+p substrings("below", dictionary)
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
