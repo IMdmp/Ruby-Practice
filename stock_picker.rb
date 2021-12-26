ba067e8dbf84c068b62cab0c51a1b508efe5a741
Dominic Pagtalunan <pagtalunan.d@gmail.com>
2021-12-26 17:12:45 +0800

    stock_picker

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,8 +1,22 @@
 # frozen_string_literal: true
 
-def stock_picker(day_arr, pair_res = Arr.new())
-  p pair_res
+def stock_picker(prices, buy_price = prices[0],buy_day = 0,profit = 0)
+  days_max_profit = [0, 0]
 
-  day_arr
+  prices.each_with_index do |price, day|
+    if price < buy_price
+      buy_price = price
+      buy_day = day
+      next
+    end
+
+    if price - buy_price > profit
+      profit = price - buy_price
+      days_max_profit = [buy_day, day]
+    end
+  end
+
+  days_max_profit
 end
-stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
+
+p stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
